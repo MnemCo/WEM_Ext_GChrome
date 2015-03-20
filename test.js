5518440748ff7689cff4bbfc9978e2fb6806fbcf
PatrickOnlineNow <pcm6c5@mst.edu>
2015-03-20 12:35:44 -0700

    icons on left. More css ids for arranging

--- test.js
+++ test.js
@@ -2,28 +2,25 @@
  * Created by Thanh on 3/10/2015.
  */
 
-chrome.tabs.getAllInWindow(null, function(tabs) {
-    tabs.forEach(function(tab) {
-       myFunction(tab.title,tab.url,tab.favIconUrl);
-    });
-});
+chrome.tabs.query({}, function(tabs) {tabs.forEach(function(tab) {myFunction(tab.title,tab.url,tab.favIconUrl);});});
 
 function myFunction(tabTitle,tabURL,tabIcon) {
     /*console.log(tabTitle);*/
-
-    var uList = document.getElementById("links");
+    var uList = document.getElementById("list");
     var newItem = document.createElement("li");
     var newItemLink = document.createElement("a");
     var favicon = document.createElement("img");
-    if(tabTitle.length > 20) tabTitle = tabTitle.substring(0,20);
+    if(tabTitle.length > 70) tabTitle = tabTitle.substring(0,70);
 
+    newItem.setAttribute("id", "listItem");
     newItemLink.textContent = tabTitle;
 
     favicon.setAttribute("src",tabIcon);
-    favicon.setAttribute("id", "link");
+    favicon.setAttribute("id", "favicons");
     newItemLink.setAttribute("href",tabURL);
+    newItemLink.setAttribute("id", "link");
 
-    newItemLink.appendChild(favicon);
+   newItemLink.appendChild(favicon);
     newItem.appendChild(newItemLink);
     uList.appendChild(newItem);
 }
